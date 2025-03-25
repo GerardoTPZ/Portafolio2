fd0f2eb0831b0b6b345c1645c0583db58a25ec8e
Gerardo Perez <gerardoadolfo18@gmail.com>
2025-03-25 03:17:18 +0000

    Primer commit - inicializando repositorio

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+$nombre_estudiante = "Gerardo Perez"; 
+
+echo "<h1>Hola mundo, soy $nombre_estudiante</h1>";
+?>
+
+<img src="Flux_Dev_A_curious_cat_with_bright_green_eyes_fluffy_white_fur_2.jpeg" alt="Imagen creada con inteligencia artificial" style="max-width: 100%; height: auto;">
